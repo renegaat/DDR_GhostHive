bf9961baa241ecb98b3179fa443aaf9a05e154a5
renegaat <joernross@gmx.net>
2017-12-30 10:57:04 +0100

    refacttoring, method renaming

--- src/main/java/net/pearlbay/service/LocationServiceImpl.java
+++ src/main/java/net/pearlbay/service/LocationServiceImpl.java
@@ -14,29 +14,26 @@ public class LocationServiceImpl implements LocationService {
 
 	@Autowired LocationDAO locationDAO;
 
-	@Override
 	public void addLocation(Location location) {
 		locationDAO.addLocation(location);
 	}
-	@Override
+
 	public List<Location> listLocation() {
 		return locationDAO.listLocation();
 	}
-	@Override
+
 	public void removeLocation(Integer id) {
 		locationDAO.removeLocation(id);	
 	}
 	
-	@Override
 	public Location getLocation(Integer id) {
 		return locationDAO.getLocation(id);
 	}
 	
-	@Override
 	public void addRichLocation(Location location) {
 		locationDAO.addRichLocation(location);
 	}
-	@Override
+
 	public List<Location> getCloseLocations(Location location) {
 		return locationDAO.getCloseLocations(location);
 	}
